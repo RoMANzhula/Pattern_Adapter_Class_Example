1398bacaf80e0f8bccf3eb737895cd4455ffb2bb
RoMANzhula <roman1985al@ukr.net>
2023-04-19 19:53:28 +0300

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,17 @@
+public class Worker { //создаем класс Работник
+    public void putTheBox() { //метод ставить коробку
+        System.out.println("I can put boxes");
+    }
+
+    public void carryTools() { //метод нести инструмент
+        System.out.println("I can carry tools");
+    }
+
+    public void liftUpMaterials() { //метод поднимать материалы
+        System.out.println("I can lift up materials");
+    }
+
+    public void twistScrews() { //метод крутить шурупы
+        System.out.println("I can twist screws");
+    }
+}
