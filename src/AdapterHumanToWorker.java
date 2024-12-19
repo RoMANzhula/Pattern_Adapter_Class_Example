926ec5b5af8756e8bbd5bc728e8831c1cac2ea80
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:44:32 +0200

    Update AdapterHumanToWorker.java
    Remove comments

--- src/AdapterHumanToWorker.java
+++ src/AdapterHumanToWorker.java
@@ -1,22 +1,21 @@
-public class AdapterHumanToWorker extends Worker implements Human { //класс адаптер, который позволяет адаптировать Человека
-    //в работника путем обьединения - т.е. реализуем в методах интерфейса методы класса
-    @Override //аннотация, которая говорит, что мы будем переопределять метод
-    public void put() { //обязательный к реализации метод (т.к. мы имплементируем интерфейс Human)
-        putTheBox(); //реализуем метод класса Worker
+public class AdapterHumanToWorker extends Worker implements Human { 
+    @Override
+    public void put() {
+        putTheBox();
     }
 
-    @Override //аннотация, которая говорит, что мы будем переопределять метод
-    public void carry() { //обязательный к реализации метод (т.к. мы имплементируем интерфейс Human)
-        carryTools(); //реализуем метод класса Worker
+    @Override
+    public void carry() { 
+        carryTools();
     }
 
-    @Override //аннотация, которая говорит, что мы будем переопределять метод
-    public void liftUp() { //обязательный к реализации метод (т.к. мы имплементируем интерфейс Human)
-        liftUpMaterials(); //реализуем метод класса Worker
+    @Override
+    public void liftUp() {
+        liftUpMaterials();
     }
 
-    @Override //аннотация, которая говорит, что мы будем переопределять метод
-    public void twist() { //обязательный к реализации метод (т.к. мы имплементируем интерфейс Human)
-        twistScrews(); //реализуем метод класса Worker
+    @Override
+    public void twist() {
+        twistScrews();
     }
 }
