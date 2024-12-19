d9336978ca44e25ce08a234e4b415e8fe4ba8354
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:45:16 +0200

    Update Human.java
    Remove comments

--- src/Human.java
+++ src/Human.java
@@ -1,9 +1,9 @@
-public interface Human { //создаем интерфейс Человек
-    public void put(); //метод ставить (что угодно) без реализации
+public interface Human {
+    public void put();
 
-    public void carry(); //метод нести (что угодно) без реализации
+    public void carry(); 
 
-    public void liftUp(); //метод поднимать (что угодно) без реализации
+    public void liftUp(); 
 
-    public void twist(); //метод крутить (что угодно) без реализации
+    public void twist(); 
 }
