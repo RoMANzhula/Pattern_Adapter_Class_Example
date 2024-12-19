6df243f18daa185a88c03330cd40bf6e29db3e17
Roman Manzhula <roman1985al@ukr.net>
2024-12-19 21:46:09 +0200

    Update Worker.java
    Remove comments

--- src/Worker.java
+++ src/Worker.java
@@ -1,17 +1,17 @@
-public class Worker { //создаем класс Работник
-    public void putTheBox() { //метод ставить коробку
+public class Worker {
+    public void putTheBox() {
         System.out.println("I can put boxes");
     }
 
-    public void carryTools() { //метод нести инструмент
+    public void carryTools() {
         System.out.println("I can carry tools");
     }
 
-    public void liftUpMaterials() { //метод поднимать материалы
+    public void liftUpMaterials() { 
         System.out.println("I can lift up materials");
     }
 
-    public void twistScrews() { //метод крутить шурупы
+    public void twistScrews() { 
         System.out.println("I can twist screws");
     }
 }
